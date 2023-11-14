d434295dd04bfbce5b44c2313277846add9c694c
AxelanO7 <jeremia123.jm@gmail.com>
2023-11-14 12:12:52 +0800

    Add header to dashboard page and center text

--- src/pages/dashboard_page.tsx
+++ src/pages/dashboard_page.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import BaseLayout from "../layouts/base";
+import HeaderPage from "../components/header_page";
 
 export default function DashboardPage() {
   return (
-    <BaseLayout padding={12}>
-      <h1>AKSES CEPAT</h1>
-      <p>silahkan klik dibawah ini</p>
+    <BaseLayout padding={12} text_color="stone_5">
+      <HeaderPage>AKSES CEPAT</HeaderPage>
+      <p className="text-center">silahkan klik dibawah ini</p>
       <div className="h-16" />
       <div className="grid grid-cols-3 gap-4 w-full h-80 text-center">
         <div className="border border-dark_green rounded-xl p-12">
